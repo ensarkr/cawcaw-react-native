8653e9775d2cf5edba0e2473c25c2edbdeed9cbd
ensarkr <eyupensarkara@gmail.com>
2023-12-20 03:30:42 +0300

    feat: add new parts to post comp
    New parts for adding comments directly from post comp. Its opt in via type  prop.

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,70 +1,107 @@
-import { Link, router } from "expo-router";
-import { post } from "../typings/database";
-import {
-  Text,
-  View,
-  StyleSheet,
-  Image,
-  Pressable,
-  ToastAndroid,
-} from "react-native";
+import { router } from "expo-router";
+import { post, postComment } from "../typings/database";
+import { View, StyleSheet, Image, Pressable, ToastAndroid } from "react-native";
 import { useState } from "react";
 import useAuth from "../context/useAuth";
 import { likeOrUnlikePostRequest } from "../functions/requests";
 import WhiteText from "./WhiteText";
+import AddComment from "./AddComment";
+import Comment from "./Comment";
 
-export default function Post({ post }: { post: post }) {
+type postComponentProps =
+  | {
+      type: "post";
+      post: post;
+      userComments?: undefined;
+      addNewUserComment?: undefined;
+    }
+  | {
+      type: "postWithComments";
+      post: post;
+      userComments: postComment[];
+      addNewUserComment: (
+        displayName: string,
+        username: string,
+        userId: number,
+        comment: string
+      ) => void;
+    };
+
+export default function Post({
+  type,
+  post,
+  userComments,
+  addNewUserComment,
+}: postComponentProps) {
   return (
     <>
-      <Pressable onPress={() => router.push("/post/" + post.id)}>
-        <View style={styles.view}>
-          <View style={styles.userView}>
-            <Pressable
-              onPress={() => {
-                router.push("/user/" + post.userId);
-              }}
-            >
-              <View>
-                <WhiteText>{post.displayName}</WhiteText>
-                <WhiteText style={styles.fade}>@{post.username}</WhiteText>
-              </View>
-            </Pressable>
-            <WhiteText style={styles.fade}>
-              {post.insertedAt.toString().split("T")[0]}
-            </WhiteText>
-          </View>
-          <WhiteText style={styles.justify}>{post.text}</WhiteText>
-          {post.imageUrl !== null && (
+      <Pressable
+        style={type === "post" ? styles.view : styles.commentView}
+        onPress={() => router.push("/post/" + post.id)}
+      >
+        <View style={styles.userView}>
+          <Pressable
+            onPress={() => {
+              router.push("/user/" + post.userId);
+            }}
+          >
+            <View>
+              <WhiteText>{post.displayName}</WhiteText>
+              <WhiteText style={styles.fade}>@{post.username}</WhiteText>
+            </View>
+          </Pressable>
+          <WhiteText style={styles.fade}>
+            {post.insertedAt.toString().split("T")[0]}
+          </WhiteText>
+        </View>
+        <WhiteText style={styles.justify}>{post.text}</WhiteText>
+        {post.imageUrl !== null && (
+          <Image
+            source={{
+              uri: post.imageUrl,
+            }}
+            style={[styles.image, { aspectRatio: post.aspectRatio }]}
+          ></Image>
+        )}
+        <View style={styles.infoView}>
+          <Pressable
+            style={styles.iconButton}
+            onPress={() => {
+              router.push("/post/" + post.id);
+            }}
+          >
             <Image
-              source={{
-                uri: post.imageUrl,
-              }}
-              style={[styles.image, { aspectRatio: post.aspectRatio }]}
+              style={styles.icon}
+              source={require("../assets/message-square-dots-regular-24.png")}
             ></Image>
-          )}
-          <View style={styles.infoView}>
-            <Pressable
-              style={styles.iconButton}
-              onPress={() => {
-                router.push("/post/" + post.id);
-              }}
-            >
-              <Image
-                style={styles.icon}
-                source={require("../assets/message-square-dots-regular-24.png")}
-              ></Image>
-              <View>
-                <WhiteText>{post.commentsCount}</WhiteText>
-              </View>
-            </Pressable>
-            <LikeButton
-              postId={post.id}
-              requestedLiked={post.requestedLiked}
-              likeCount={post.likesCount}
-            ></LikeButton>
-          </View>
+            <View>
+              <WhiteText>
+                {userComments !== undefined
+                  ? userComments.length + post.commentsCount
+                  : post.commentsCount}
+              </WhiteText>
+            </View>
+          </Pressable>
+          <LikeButton
+            postId={post.id}
+            requestedLiked={post.requestedLiked}
+            likeCount={post.likesCount}
+          ></LikeButton>
         </View>
       </Pressable>
+      {type === "postWithComments" ? (
+        <>
+          <AddComment
+            postId={post.id}
+            addNewUserComment={addNewUserComment}
+          ></AddComment>
+          {userComments.map((e) => (
+            <Comment key={e.id} comment={e}></Comment>
+          ))}
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
@@ -98,7 +135,7 @@ function LikeButton({
     );
 
     if (!res.status) {
-      setLiked((pv) => !pv);
+      if (!res.message.includes("Already")) setLiked((pv) => !pv);
       ToastAndroid.show(res.message, 200);
     }
   };
@@ -134,8 +171,15 @@ function LikeButton({
 }
 
 const styles = StyleSheet.create({
+  commentView: {
+    padding: 16,
+    borderColor: "gray",
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    color: "white",
+    gap: 15,
+  },
   view: {
-    flex: 1,
     padding: 15,
     borderRadius: 5,
     borderColor: "gray",
@@ -146,7 +190,6 @@ const styles = StyleSheet.create({
   userView: {
     flexDirection: "row",
     justifyContent: "space-between",
-    flex: 1,
   },
   icon: {
     height: 20,
